014bea71e9132133c7d83f96621e4dd22d6496d4
k1713310 <64620303+k1713310@users.noreply.github.com>
2021-01-25 12:47:43 -0600

    Add files via upload

---
+++ Python TestScript.py
@@ -0,0 +1,39 @@
+print()
+print ("Hello World")
+fred = 200
+print(fred)
+john = fred
+print(john)
+print()
+number_of_coins = 240
+print(number_of_coins) 
+found_coins = 20
+magic_coins = 10
+stolen_coins = 3
+found_coins + magic_coins * 365 - stolen_coins * 52
+stolen_coins = 2
+magic_coins = 13
+found_coins + magic_coins * 365 - stolen_coins * 52
+fred = "why do gorillas have big nostrils? Big fingers!!"
+print (fred)
+fred = "what is pink and fluffy? Pink fluff!"
+print (fred)
+fred = '''how do dinosaurs pay their bills?
+With tyrannosaurus checks!'''
+print (fred)
+silly_string = '''He said, "Aren't can't shoudn't wouldn't ."'''
+print (silly_string)
+joke_text = '%s: a device for finding furniture in the dark'
+bodypart1 = 'knee'
+bodypart2 = 'shin'
+print(joke_text % bodypart1)
+print(joke_text % bodypart2)
+myscore = 2400
+message = 'I scored %s points'
+print(message % myscore)
+nums = 'What did number %s say to number %s? Nice Belt!!'
+print(nums % (0, 8))
+print(10 * 'Hello ')
+print (1000 * 'WOW! ')
+
+
